a6062bdac8cbf4c061436b572ac626672d8e19ca
nikhilkumbhare <nikhilk190697@gmail.com>
2021-03-01 13:25:19 +0530

    Added: Ability for user to make

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,24 +1,35 @@
 import java.util.*;
 
-class TicTacToe {
+public class TicTacToe {
 
-	public static char[] board = new char[10];
+	static Scanner userInput = new Scanner(System.in);
+	static char userLetter;
+	static char computerLetter;
+	static char[] board = new char[10];
 
-	public char[] createboard(){
+	public char[] createBoard() {
 
-		for (  int i=0 ; i < 10; i++ ){
+		for (  int i=1 ; i < 10; i++ ) {
 			board[i] = ' ';
 		}
 		return board;
 	}
 
-	public static char chooseLetter(Scanner userInput) {
+	public static void chooseLetter() {
 
 		System.out.print("Choose your Letter X or O : ");
-		return userInput.next().toUpperCase().charAt(0);
+		userLetter = userInput.next().toUpperCase().charAt(0);
+
+		if (userLetter == 'X' || userLetter == 'x' || userLetter == 'o' || userLetter == 'O') {
+			computerLetter = (userLetter == 'X') ? 'O' : 'X';
+		} else {
+			System.out.println("Invalid INPUT");
+			chooseLetter();
+		}
+
 	}
 
-	static void showBoard(){
+	public static void showBoard(){
 
 		System.out.println( "    " +board[1]+ "   | "+ board[2]+ "  | " + board[3]+ "     ");
 		System.out.println(" ------------------------ ");
@@ -27,20 +38,24 @@ class TicTacToe {
 		System.out.println( "    " +board[7]+ "   | "+ board[8]+ "  | " + board[9]+ "     ");
 	}
 
+	public static void makeMove() {
+
+		System.out.print("Select location between 1-9 : ");
+		int location = userInput.nextInt();
+		if (board[location] != ' ') {
+			System.out.println("Position Already Occupied");
+		}
+	}
 
 	public static void main(String[] args){
 
 		System.out.println("Welcome to TicTacToeGame");
 
-		Scanner userInput = new Scanner(System.in);
-		char userLetter = chooseLetter(userInput);
-		char computerLetter = (userLetter == 'X') ? 'O' : 'X';
-
-		System.out.println("User Letter: " +userLetter);
-		System.out.println("Computer Letter: " +computerLetter);
-
 		TicTacToe ticTacToeGame = new TicTacToe();
-		ticTacToeGame.createboard();
+		ticTacToeGame.createBoard();
+		chooseLetter();
+      System.out.println("Computer Letter: " +computerLetter);
 		showBoard();
+		makeMove();
 	}
 }
